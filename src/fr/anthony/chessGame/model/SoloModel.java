ac9a475150506f009a963756823968fe52971898
Anthony fauveau <anthony.fv@hotmail.com>
2020-04-02 10:46:07 +0200

    code cleaning

--- src/fr/anthony/chessGame/model/SoloModel.java
+++ src/fr/anthony/chessGame/model/SoloModel.java
@@ -2,19 +2,11 @@ package fr.anthony.chessGame.model;
 
 public class SoloModel {
 	
-	private PositionModel[][] position = new PositionModel[8][8];
 
     public SoloModel() {
         
-        for(int i=0; i<position.length; i++){
-            for(int j=0; j<position.length; j++){
-                this.position[i][j] = new PositionModel(i, j);
-            }
-        }
+       
     }
 
-    public PositionModel getSpot(int x, int y) {
-        return position[x][y];
-    }
 }
  
